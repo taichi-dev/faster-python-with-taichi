29f68e428b27b93ec7a6e2feeda1feec80f59485
Yuanming Hu <yuanmhu@gmail.com>
2022-07-27 11:46:08 +0800

    Update lcs.py

--- lcs.py
+++ lcs.py
@@ -10,8 +10,8 @@ N = 15000
 f = ti.field(dtype=ti.i32, shape=(N + 1, N + 1))
 
 if benchmark:
-    a_numpy = np.random.randint(0, 10, N, dtype=np.int32)
-    b_numpy = np.random.randint(0, 10, N, dtype=np.int32)
+    a_numpy = np.random.randint(0, 100, N, dtype=np.int32)
+    b_numpy = np.random.randint(0, 100, N, dtype=np.int32)
 else:
     a_numpy = np.array([0, 1, 0, 2, 4, 3, 1, 2, 1], dtype=np.int32)
     b_numpy = np.array([4, 0, 1, 4, 5, 3, 1, 2], dtype=np.int32)
@@ -24,11 +24,8 @@ def compute_lcs(a: ti.types.ndarray(), b: ti.types.ndarray()) -> ti.i32:
     ti.loop_config(serialize=True)
     for i in range(1, len_a + 1):
         for j in range(1, len_b + 1):
-            same = 0
-            if a[i - 1] == b[j - 1]:
-                same = 1
-            f[i, j] = max(f[i - 1, j - 1] + same, max(f[i - 1, j], f[i,
-                                                                     j - 1]))
+            f[i, j] = max(f[i - 1, j - 1] + (a[i - 1] == b[j - 1]),
+                          max(f[i - 1, j], f[i, j - 1]))
 
     return f[len_a, len_b]
 
